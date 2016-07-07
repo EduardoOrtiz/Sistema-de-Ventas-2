1619fd6af720a91536b6dc274e8a95fccf28d82d
EduardoOrtiz <eduardoortizm96@gmail.com>
2016-07-07 00:40:12 -0500

    Update PruebaHitHub.java
    correccion

--- PruebaHitHub/src/pruebahithub/PruebaHitHub.java
+++ PruebaHitHub/src/pruebahithub/PruebaHitHub.java
@@ -5,7 +5,7 @@ public class PruebaHitHub {
     public static void main(String[] args) {
         
         System.out.println("Hola commit");
-        Sistema.Cabo.Println("Cambio desde git hub");
+        System.out.rintln("Cambio desde git hub");
     }
 
 }
